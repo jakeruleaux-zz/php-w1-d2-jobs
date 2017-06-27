9418d79925c0d247bcaee105fef83635511f0651
Jacob Ruleaux and Nicole Freed <nicole@>
2017-06-27 10:23:57 -0700

    builds boiler plates for files

---
+++ app/app.php
@@ -0,0 +1,14 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/JobOpening.php";
+
+    $app = new Silex\Application();
+
+    $app->get("/", function() {
+
+        return $output;
+    });
+
+
+    return $app;
+?>
