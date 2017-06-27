972bf1c0b6c3968f833eefd07e9f3beb83c8fb9b
Jacob Ruleaux and Nicole Freed <jacob@>
2017-06-27 10:41:26 -0700

    create job posting form

--- app/app.php
+++ app/app.php
@@ -6,7 +6,34 @@
 
     $app->get("/", function() {
 
-        return $output;
+        return "
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Job Openings</title>
+            </head>
+            <body>
+                <div class='container'>
+                    <form action='/job-listings' method='post'>
+                        <div class='form-group'>
+                            <label for='title'>Job Title: </label>
+                            <input type='text' name='title'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='description'>Description: </label>
+                            <input type='text' name='description'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='contact'>Contact Person: </label>
+                            <input type='text' name='contact'>
+                        </div>
+                        <button type='submit' name='submit'>Post</button>
+                    </ form>
+                </div>
+            </body>
+        </html>
+        "
+        ;
     });
 
 
