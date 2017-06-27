6b567a654b8e8af717fb11f0a4b71008ebce7906
Jacob Ruleaux and Nicole Freed <jacob@>
2017-06-27 13:06:44 -0700

    build and debug form handler

--- app/app.php
+++ app/app.php
@@ -14,7 +14,7 @@
             </head>
             <body>
                 <div class='container'>
-                    <form action='/job-listings' method='post'>
+                    <form action='/job-listings'>
                         <div class='form-group'>
                             <label for='title'>Job Title: </label>
                             <input type='text' name='title'>
@@ -35,7 +35,27 @@
         "
         ;
     });
-    $output = "";
+
+    $app->get("/job-listings", function() {
+        $new_job = new JobOpening($_GET['title'], $_GET['description'], $_GET['contact']);
+        $new_job_title = $new_job->getTitle();
+        $new_job_description = $new_job->getDescription();
+        $new_job_contact = $new_job->getContact();
+
+        return "
+        <div class='container'>
+            <h1>Looking for:</h1>
+            <ul>
+                <li><strong>Job Title:</strong> " . $new_job_title . "</li>
+                <li><strong>Description:</strong> " . $new_job_description . "</li>
+                <li><strong>Contact:</strong> ". $new_job_contact . "</li>
+            </ul>
+        </div>
+        "
+        ;
+
+        // return $output;
+    });
 
     return $app;
 ?>
--- src/JobOpening.php
+++ src/JobOpening.php
@@ -4,46 +4,44 @@
         private $title;
         private $description;
         private $contact;
+
+        function __construct($title, $description, $contact)
+        {
+            $this->title = $title;
+            $this->description = $description;
+            $this->contact = $contact;
+        }
+
+        function getTitle()
+        {
+            return $this->title;
+        }
+
+        function getDescription()
+        {
+            return $this->description;
+        }
+
+        function getContact()
+        {
+            return $this->contact;
+        }
+
+        function setTitle()
+        {
+            $this->title = $new_title;
+        }
+
+        function setDescription()
+        {
+            $this->title = $new_description;
+        }
+
+        function setContact()
+        {
+            $this->title = $new_contact;
+        }
     }
-
-    function __construct($inputted_title, $inpuuted_description, $inputted_contact)
-    {
-        $this->title = $inputted_title;
-        $this->description = $inpuuted_description;
-        $this->contact = $inputted_contact;
-    }
-
-    function getTitle()
-    {
-        return $this->title;
-    }
-
-    function getDescription()
-    {
-        return $this->description;
-    }
-
-    function getContact()
-    {
-        return $this->contact;
-    }
-
-    function setTitle()
-    {
-        $this->title = $new_title;
-    }
-
-    function setDescription()
-    {
-        $this->title = $new_description;
-    }
-
-    function setContact()
-    {
-        $this->title = $new_contact;
-    }
-
-
 
 
 ?>
